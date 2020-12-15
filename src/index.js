3222ca6d1545b2fdb74ad11a455dc355ca4f9408
kkwoncokr <kkwoncokr@gmail.com>
2020-12-15 17:16:37 +0900

    BUG : https bug..

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const app = new Koa();
 const router = new Router();
 
 app.use(cors({
-  origin: 'https://kkwon.me',
+  origin: 'http://blog.kkwon.me',
   credentials: true,
   exposeHeaders:'Last-page',
 }))
